0cf4cf5689cefaedd891afb94c5ab8c1aa5e5f1b
Qu Jiayou <1012666754@qq.com>
2018-09-05 17:17:14 +0100

    Initial Commit

---
+++ SupChatsTests/SupChatsTests.swift
@@ -0,0 +1,36 @@
+//
+//  SupChatsTests.swift
+//  SupChatsTests
+//
+//  Created by QJY on 05/09/2018.
+//  Copyright © 2018 QJY. All rights reserved.
+//
+
+import XCTest
+@testable import SupChats
+
+class SupChatsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
